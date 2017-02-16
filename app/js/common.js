2e5611c9a3d35523e7f52e6c48c096ae301c5853
unknown <C:\Users\Admin\AppData\Roaming\The Bat!>
2017-02-16 09:55:22 +0300

    slider-header

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,9 @@
 $(function() {
 
-	// Custom JS
+	$(".slider-header").slick({
+		arrows: true,
+		prevArrow: '<div class="slider-nav nav-prev"></div>',
+		nextArrow: '<div class="slider-nav nav-next"></div>',
+	});
 
 });
