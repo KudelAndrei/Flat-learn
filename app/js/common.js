3eb9f54ac63557314cc501c36a5d5eded82c3e02
unknown <C:\Users\Admin\AppData\Roaming\The Bat!>
2017-02-16 17:05:01 +0300

    menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,11 @@
 $(function() {
 
+	$(".toggle-menu").click(function(){
+		$(this).parent().toggleClass("active");
+		$(".menu-wrap").css({'left': '0'});
+		$(".menu").slideToggle();
+	});
+
 	$(".slider-header").slick({
 		arrows: true,
 		prevArrow: '<div class="slider-nav nav-prev"></div>',
