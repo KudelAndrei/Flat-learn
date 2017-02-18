94d95c9284ce69f812e251c9d01819f4a5e6ac10
KudelAndrei <kudel1995@mail.ru>
2017-02-19 00:35:43 +0300

    responsive mobile

--- app/js/common.js
+++ app/js/common.js
@@ -1,10 +1,20 @@
 $(function() {
 
 	$(".toggle-menu").click(function(){
-		$(this).parent().toggleClass("active");
-		$(".menu-wrap").css({'left': '0'});
+		$(this).siblings().toggleClass("active");
 		$(".menu").slideToggle();
 	});
+
+	$(".toggle-close").click(function(){
+		$(".menu-wrap").toggleClass("active");
+		$(".menu").hide(1);
+	});
+
+	$(".menu-wrap .menu").click(function(){
+		$(".menu-wrap").toggleClass("active");
+		$(".menu").hide(1);
+	});
+
 
 	$(".search i").click(function(){
 		$(".search").toggleClass("on");
@@ -15,7 +25,15 @@ $(function() {
 		arrows: true,
 		prevArrow: '<div class="header-nav nav-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
 		nextArrow: '<div class="header-nav nav-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>',
-		asNavFor: '.header-nav'
+		asNavFor: '.header-nav',
+		responsive: [
+		 {
+			breakpoint: 1150,
+			settings: {
+				infinite: true,
+				arrows: false,
+			}}
+		]
 	});
 		$('.header-nav').slick({
 		slidesToShow: 2,
@@ -24,7 +42,26 @@ $(function() {
 		dots: false,
 		prevArrow: '<div class="header-navigation navigation-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
 		nextArrow: '<div class="header-navigation navigation-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>',
-		focusOnSelect: true
+		focusOnSelect: true,
+		 responsive: [
+		 {
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 2,
+				infinite: true,
+			},
+			breakpoint: 880,
+			settings: {
+				arrows: false,
+			}
+		 }]
+	});
+
+	$(".bottom").click(function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+		top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 500);
 	});
 
 
